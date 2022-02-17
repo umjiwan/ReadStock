37acb23b6e1c57f6d7f2c6a8177458f188bea7d5
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-02-18 01:03:37 +0900

    Add Compare func ;)

--- ReadStock.py
+++ ReadStock.py
@@ -6,6 +6,7 @@ https://github.com/umjiwan/ReadStock.git
 
 import requests
 from bs4 import BeautifulSoup
+from soupsieve import select
 
 class StockData: # 주식 데이터 클래스
     def __init__(self, StockCode): # StockCode = 주식 종목코드
@@ -16,6 +17,15 @@ class StockData: # 주식 데이터 클래스
 
         self.soup = soup
         self.StockCode = StockCode
+
+    def StockName(self): # 주식 종목 이름 가져오는 함수
+        selector = "#middle > div.h_company > div.wrap_company > h2 > a"
+        result = self.soup.select_one(selector)
+
+        if not result: # 해당 종목이 없을 경우
+            return None  
+
+        return result.text
 
     def NowPrice(self, shim=False): # 현재가 가져오는 함수
         selector = "#chart_area > div.rate_info > div > p.no_today > em > span"
@@ -31,3 +41,39 @@ class StockData: # 주식 데이터 클래스
             return result.text
         else:
             return int(result.text.replace(",", ""))
+
+    def Compare(self, shim=False, percent=False): # 전일대비 가져오는 함수
+        if not percent:
+            selector = "#chart_area > div.rate_info > div > p.no_exday > em > span.blind"
+            result = self.soup.select_one(selector)
+
+            if not result: # 해당 종목이 없을 경우
+                return None
+
+            selector = "#chart_area > div.rate_info > div > p.no_exday > em:nth-child(2) > span"
+            updown = self.soup.select_one(selector).text
+
+            if updown != "상승":
+                result = "-" + result.text
+            else:
+                result = "+" + result.text
+
+            if shim:
+                return result
+            else:
+                return int(result.replace(",", ""))
+
+        elif percent:
+            selector = "#chart_area > div.rate_info > div > p.no_exday > em:nth-child(4) > span.blind"
+            result = self.soup.select_one(selector)
+
+            if not result:
+                return None
+
+            selector = "#chart_area > div.rate_info > div > p.no_exday > em:nth-child(4) > span"
+            updown = self.soup.select_one(selector).text
+
+            if updown != "-":
+                updown = "+"
+
+            return updown + result.text + "%"
