2885265375bceb8c7902259509bcaf3ff8278202
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-02-17 20:29:14 +0900

    주석 추가 💎

--- ReadStock.py
+++ ReadStock.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-class StockData:
-    def __init__(self, StockCode):
+class StockData: # 주식 데이터 클래스
+    def __init__(self, StockCode): # StockCode = 주식 종목코드
         url = "https://finance.naver.com/item/main.naver?code=" + StockCode
         response = requests.get(url)
         html = response.text
@@ -11,9 +11,12 @@ class StockData:
         self.soup = soup
         self.StockCode = StockCode
 
-    def NowPrice(self, shim=False):
+    def NowPrice(self, shim=False): # 현재가 가져오는 함수
         selector = "#chart_area > div.rate_info > div > p.no_today > em > span"
         result = self.soup.select_one(selector).text
+
+        # shim은 현재가를 문자열로 쉼표를 붙여서 출력하는 옵션
+        # True가 쉼표가 있는것
 
         if shim:
             return result
