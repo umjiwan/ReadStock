261e0efb6e9e69af02cd50316b6d5fe9acd1a18a
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-02-17 21:20:41 +0900

    commit

--- ReadStock.py
+++ ReadStock.py
@@ -19,12 +19,15 @@ class StockData: # 주식 데이터 클래스
 
     def NowPrice(self, shim=False): # 현재가 가져오는 함수
         selector = "#chart_area > div.rate_info > div > p.no_today > em > span"
-        result = self.soup.select_one(selector).text
+        result = self.soup.select_one(selector)
+
+        if not result: # 해당 종목이 없을 경우
+            return None
 
         # shim은 현재가를 문자열로 쉼표를 붙여서 출력하는 옵션
         # True가 쉼표가 있는것
 
         if shim:
-            return result
+            return result.text
         else:
-            return int(result.replace(",", ""))
+            return int(result.text.replace(",", ""))
